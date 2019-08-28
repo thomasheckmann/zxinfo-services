86e04e424addc2a1624a422f3fb652a274e3ad44
Thomas Ahn Kolbeck Kjær Heckmann <thomas.kolbeck@gmail.com>
2019-08-28 15:39:14 +0200

    thin API access

--- app.js
+++ app.js
@@ -16,14 +16,16 @@ var cors = require('cors');
 var neo4j = require('./routes/neo4j');
 var social = require('./routes/social');
 
+var flatten = require('flat')
+
 var appConfig = require('./config.json');
 
 var app = express();
 app.use(cors());
 
 if (process.env.NODE_ENV === undefined) {
-  console.log("NODE_ENV not defined, must be 'development' or 'production'");
-  process.exit(0);
+    console.log("NODE_ENV not defined, must be 'development' or 'production'");
+    process.exit(0);
 }
 
 console.log('# APP START ###################################################');
@@ -43,7 +45,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -56,11 +58,30 @@ app.use('/api/zxinfo/v2/magazines', magazinesAPI);
 app.use('/api/graph', neo4j);
 app.use('/social', social);
 
+var proxy = require('express-http-proxy');
+app.use('/a.proxy', proxy('http://localhost:8300', {
+    userResDecorator: function(proxyRes, proxyResData, userReq, userRes) {
+        data = flatten(JSON.parse(proxyResData.toString('utf8')));
+
+        // TODO: Handle 404
+
+        result = "";
+        for (let [key, value] of Object.entries(data)) {
+            if (key.startsWith('_source.') && value.length > 0) {
+                result += key.replace('_source.', '') + "=" + value + "\n";
+                console.log(`${key}: ${value}` + '(' + value.length + ')');
+            }
+        }
+
+        return result;
+    }
+}));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -68,35 +89,35 @@ app.use(function(req, res, next) {
 // container error handler
 // will print stacktrace
 if (app.get('env') === 'container') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
