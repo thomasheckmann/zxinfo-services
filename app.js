cf03299993360ff106f52e3225f8dc189b331af9
Thomas Ahn Kolbeck Kjær Heckmann <thomas.kolbeck@gmail.com>
2020-04-28 11:06:46 +0200

    Added suggestion API

--- app.js
+++ app.js
@@ -1,139 +1,143 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var flatten = require('flat')
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var flatten = require("flat");
 
-var routes = require('./routes/index');
+var routes = require("./routes/index");
 
-var zxinfo = require('./routes/zxinfo');
-var zxinfov2 = require('./routes/zxinfov2');
-var magazinesAPI = require('./routes/magazinesAPI');
+var zxinfo = require("./routes/zxinfo");
+var zxinfov2 = require("./routes/zxinfov2");
+var magazinesAPI = require("./routes/magazinesAPI");
 
-var zxsuggest = require('./routes/zxsuggest');
-var cors = require('cors');
-var neo4j = require('./routes/neo4j');
-var social = require('./routes/social');
-var proxy = require('express-http-proxy');
+var zxsuggest = require("./routes/zxsuggest");
+var cors = require("cors");
+var neo4j = require("./routes/neo4j");
+var social = require("./routes/social");
+var proxy = require("express-http-proxy");
 
-var appConfig = require('./config.json');
+var appConfig = require("./config.json");
 
 var app = express();
 app.use(cors());
 
 if (process.env.NODE_ENV === undefined) {
-    console.log("NODE_ENV not defined, must be 'development' or 'production'");
-    process.exit(0);
+  console.log("NODE_ENV not defined, must be 'development' or 'production'");
+  process.exit(0);
 }
 
-console.log('# APP START ###################################################');
-console.log('# RUNNING in mode: ' + process.env.NODE_ENV);
-console.log('# nodeJS version: ' + process.version);
-console.log('#');
-console.log('# CONFIG DUMP #################################################');
+console.log("# APP START ###################################################");
+console.log("# RUNNING in mode: " + process.env.NODE_ENV);
+console.log("# nodeJS version: " + process.version);
+console.log("#");
+console.log("# CONFIG DUMP #################################################");
 console.log(JSON.stringify(appConfig[process.env.NODE_ENV], null, 2));
-console.log('###############################################################');
+console.log("###############################################################");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
-app.use('/api/zxinfo', zxinfo);
-app.use('/api/zxinfo/v2', zxinfov2);
-app.use('/api/zxinfo/v2/magazines', magazinesAPI);
-// app.use('/api/zxsuggest', zxsuggest);
-app.use('/api/graph', neo4j);
-app.use('/social', social);
+app.use("/", routes);
+app.use("/api/zxinfo", zxinfo);
+app.use("/api/zxinfo/v2", zxinfov2);
+app.use("/api/zxinfo/v2/magazines", magazinesAPI);
+app.use("/api/suggest", zxsuggest);
+app.use("/api/graph", neo4j);
+app.use("/social", social);
 
 //app.use('/a.proxy', proxy('http://localhost:8300', {
-app.use('/a.proxy', proxy('https://api.zxinfo.dk', {
-    userResDecorator: function(proxyRes, proxyResData, userReq, userRes) {
-        data = flatten(JSON.parse(proxyResData.toString('utf8')));
+app.use(
+  "/a.proxy",
+  proxy("https://api.zxinfo.dk", {
+    userResDecorator: function (proxyRes, proxyResData, userReq, userRes) {
+      data = flatten(JSON.parse(proxyResData.toString("utf8")));
 
-        // TODO: Handle 404 from API
-        result = "";
-        if (userReq.path.startsWith('/api/zxinfo/games/')) {
-            for (let [key, value] of Object.entries(data)) {
-                if (key.startsWith('_source.')) {
-                    result += key.replace('_source.', '') + "=" + value + "\n";
-                }
-            }
-        } else if (userReq.path.startsWith('/api/zxinfo/v2/search')) {
-            for (let [key, value] of Object.entries(data)) {
-                if (key.startsWith('hits.')) {
-                    result += key.replace('hits.', '').replace('_source.', '') + "=" + value + "\n";
-                    // console.log(`${key}: ${value}` + '(' + value.length + ')');
-                }
-            }
-        } else {
-            result = userReq.path + "\n" + JSON.stringify(data);
+      // TODO: Handle 404 from API
+      result = "";
+      if (userReq.path.startsWith("/api/zxinfo/games/")) {
+        for (let [key, value] of Object.entries(data)) {
+          if (key.startsWith("_source.")) {
+            result += key.replace("_source.", "") + "=" + value + "\n";
+          }
         }
+      } else if (userReq.path.startsWith("/api/zxinfo/v2/search")) {
+        for (let [key, value] of Object.entries(data)) {
+          if (key.startsWith("hits.")) {
+            result += key.replace("hits.", "").replace("_source.", "") + "=" + value + "\n";
+            // console.log(`${key}: ${value}` + '(' + value.length + ')');
+          }
+        }
+      } else {
+        result = userReq.path + "\n" + JSON.stringify(data);
+      }
 
-        console.log("OUT: " + proxyRes.headers['content-type']);
-        console.log("OUT: " + proxyRes.headers['content-encoding']);
-        return result;
+      console.log("OUT: " + proxyRes.headers["content-type"]);
+      console.log("OUT: " + proxyRes.headers["content-encoding"]);
+      return result;
     },
     userResHeaderDecorator(headers, userReq, userRes, proxyReq, proxyRes) {
-        headers['content-type'] = 'text/plain;charset=UTF-8';
-	    return headers;
-  	}
-}));
+      headers["content-type"] = "text/plain;charset=UTF-8";
+      return headers;
+    },
+  })
+);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
 
 // container error handler
 // will print stacktrace
-if (app.get('env') === 'container') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+if (app.get("env") === "container") {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err,
     });
+  });
 }
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+if (app.get("env") === "development") {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err,
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: {},
+  });
 });
-
 
 module.exports = app;
--- routes/zxsuggest.js
+++ routes/zxsuggest.js
@@ -1,52 +1,117 @@
-var config = require('../config.json')[process.env.NODE_ENV || 'development'];
-var express = require('express');
+var config = require("../config.json")[process.env.NODE_ENV || "development"];
+var express = require("express");
 var router = express.Router();
 
-var elasticsearch = require('elasticsearch');
+var elasticsearch = require("elasticsearch");
+var debug = require("debug")("zxinfo-services:suggest");
 
 var elasticClient = new elasticsearch.Client({
-    host: config.es_host,
-    apiVersion: config.es_apiVersion,
-    log: config.log
+  host: config.es_host,
+  apiVersion: config.es_apiVersion,
+  log: config.es_log,
 });
 
-var es_index = "zxinfo_suggests";
-var es_index_type = "zxinfo_suggests_type_title";
+var es_index = config.zxinfo_index;
 
 var maxResult = 20;
 
-var getSuggestions = function(q) {
-    return elasticClient.suggest({
-        index: es_index,
-        type: es_index_type,
-        body: {
-            productsuggest: {
-                text: q,
-                completion: {
-                    field: "suggest",
-                    size: maxResult
-                }
-            }
-        }
-    })
+var getSuggestions = function (query) {
+  return elasticClient.search({
+    index: es_index,
+    body: {
+      _source: ["fulltitle", "contenttype", "metadata_author"],
+      suggest: {
+        text: query,
+        titles: {
+          completion: {
+            field: "titlesuggest",
+            skip_duplicates: true,
+            size: 4,
+          },
+        },
+        authors: {
+          completion: {
+            field: "authorsuggest",
+            skip_duplicates: true,
+            size: 4,
+          },
+        },
+      },
+    },
+  });
+};
+
+var prepareSuggestions = function (result) {
+  function uniq(a, param) {
+    return a.filter(function (item, pos, array) {
+      return (
+        array
+          .map(function (mapItem) {
+            return mapItem[param];
+          })
+          .indexOf(item[param]) === pos
+      );
+    });
+  }
+
+  var suggestons = [];
+
+  // iterate titles
+  var i = 0;
+  for (; i < result.suggest.titles[0].options.length; i++) {
+    var item = {
+      text: result.suggest.titles[0].options[i]._source.fulltitle,
+      type: result.suggest.titles[0].options[i]._source.contenttype,
+      entry_id: result.suggest.titles[0].options[i]._id,
+    };
+    suggestons.push(item);
+  }
+
+  // iterate authors
+  var aut_suggestions = [];
+  var j = 0;
+  for (; j < result.suggest.authors[0].options.length; j++) {
+    var names = result.suggest.authors[0].options[j]._source.metadata_author;
+    var text = result.suggest.authors[0].options[j].text;
+
+    var output = text;
+    var t = 0;
+    for (; t < names.length; t++) {
+      if (names[t].alias.indexOf(text) > -1) {
+        output = names[t].name;
+      }
+    }
+    var item = { text: output, type: "AUTHOR" };
+    aut_suggestions.push(item);
+  }
+  // aut_suggestions = removeDuplicates(aut_suggestions, "output");
+  aut_suggestions = uniq(aut_suggestions, "output");
+
+  suggestons.push.apply(suggestons, aut_suggestions);
+
+  // sort
+  suggestons.sort(function (a, b) {
+    return a.output - b.output;
+  });
+
+  return suggestons;
 };
 
 // middleware to use for all requests
-router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    // do logging
-    next(); // make sure we go to the next routes and don't stop here
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // do logging
+  next(); // make sure we go to the next routes and don't stop here
 });
 
 /* GET title suggestions for completion */
-router.get('/suggest/:query', function(req, res, next) {
-    var suggestions = null;
-    getSuggestions(req.params.query).then(function(result) {
-        suggestions = result.productsuggest[0].options;
-        res.send(suggestions);
-    });
+router.get("/:query", function (req, res, next) {
+  var suggestions = null;
+  getSuggestions(req.params.query).then(function (result) {
+    res.send(prepareSuggestions(result));
+    //res.send(result);
+  });
 });
 
 module.exports = router;
-
