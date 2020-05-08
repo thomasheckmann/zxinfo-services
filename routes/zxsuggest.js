c7c9db7296bd17924c8443c6ec451c1883bf2e47
Thomas Ahn Kolbeck Kjær Heckmann <thomas.kolbeck@gmail.com>
2020-05-08 13:30:33 +0200

    added suggestion endpoint for names

--- routes/zxsuggest.js
+++ routes/zxsuggest.js
@@ -97,6 +97,93 @@ var prepareSuggestions = function (result) {
   return suggestons;
 };
 
+/*
+
+curl -XPOST "https://search.zxinfo.dk/zxinfo_games/_search" -H 'Content-Type: application/json' -d'
+{
+   "_source":[
+      "metadata_author"
+   ],
+   "suggest":{
+      "text":"olav",
+      "authors":{
+         "completion":{
+            "field":"authorsuggest",
+            "skip_duplicates":true,
+            "size":10
+         }
+      }
+   }
+}'|json_pp
+
+*/
+var getAuthorSuggestions = function (name) {
+  console.log("getAuthorSuggestions(): " + name);
+  return elasticClient.search({
+    index: es_index,
+    body: {
+      _source: ["metadata_author"],
+      suggest: {
+        text: name,
+        authors: {
+          completion: {
+            field: "authorsuggest",
+            skip_duplicates: true,
+            size: 10,
+          },
+        },
+      },
+    },
+  });
+};
+
+var prepareAuthorSuggestions = function (result) {
+  console.log(result);
+  var suggestons = [];
+
+  function uniq(a, param) {
+    return a.filter(function (item, pos, array) {
+      return (
+        array
+          .map(function (mapItem) {
+            return mapItem[param];
+          })
+          .indexOf(item[param]) === pos
+      );
+    });
+  }
+
+  // iterate suggest.authors.options
+  for (var j = 0; j < result.suggest.authors[0].options.length; j++) {
+    var names = result.suggest.authors[0].options[j]._source.metadata_author;
+    var text = result.suggest.authors[0].options[j].text;
+
+    var output = text;
+    var t = 0;
+    for (; t < names.length; t++) {
+      if (names[t].alias.indexOf(text) > -1) {
+        output = names[t].name;
+      }
+    }
+    var item = { text: output, type: "AUTHOR" };
+    suggestons.push(item);
+    suggestons.push(item);
+  }
+
+  // sort
+  suggestons.sort(function (a, b) {
+    return a.output - b.output;
+  });
+  // suggestons = removeDuplicates(suggestons, "output");
+  suggestons = uniq(suggestons, "text");
+
+  return suggestons;
+};
+
+/*******************************************
+ * ROUTES - ENDPOINTS
+ *******************************************/
+
 // middleware to use for all requests
 router.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -105,11 +192,20 @@ router.use(function (req, res, next) {
   next(); // make sure we go to the next routes and don't stop here
 });
 
-/* GET title suggestions for completion */
+/* GET title suggestions for completion (all) */
 router.get("/:query", function (req, res, next) {
   var suggestions = null;
   getSuggestions(req.params.query).then(function (result) {
     res.send(prepareSuggestions(result));
+    //res.send(result);
+  });
+});
+
+/* GET title suggestions for names (authors) */
+router.get("/author/:name", function (req, res, next) {
+  var suggestions = null;
+  getAuthorSuggestions(req.params.name).then(function (result) {
+    res.send(prepareAuthorSuggestions(result));
     //res.send(result);
   });
 });
