aede02eea37a80f1bc70f3549a15bc4e22e22d45
Thomas Ahn Kolbeck Kjær Heckmann <thomas.kolbeck@gmail.com>
2020-08-07 23:05:06 +0200

    x

--- routes/scr.js
+++ routes/scr.js
@@ -38,7 +38,6 @@ router.use(function (req, res, next) {
 router.post("/upload", upload.single("file"), (req, res) => {
   debug("==> /upload - " + JSON.stringify(req.file));
 
-  console.log("UPLOAD: " + __dirname);
   // load BMP
   Jimp.read(req.file.path, (err, image) => {
     if (err) throw err;
